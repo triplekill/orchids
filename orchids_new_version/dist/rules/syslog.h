33990bb49dbed7a1606880f74e1f96e7af180b7f
goubault <goubault@86f71658-340e-0410-bfa3-946ef8bbe4df>
2015-01-20 13:03:56 +0000

    Added syslog.h, defining severity and facility constants in the syslog format.  Before those were reported as strings.  The future version of syslog will report them as uints.

---
+++ orchids_new_version/dist/rules/syslog.h
@@ -0,0 +1,66 @@
+
+#define SYSLOG_SEVERITY_EMERG 0
+/* (0) Emergency: system is unusable */
+#define SYSLOG_SEVERITY_ALERT 1
+/* (1) Alert: action must be taken immediately */
+#define SYSLOG_SEVERITY_CRIT 2
+/* (2) Critical: critical conditions */
+#define SYSLOG_SEVERITY_ERR 3
+/* (3) Error: error conditions */
+#define SYSLOG_SEVERITY_WARN
+/* (4) Warning: warning conditions */
+#define SYSLOG_SEVERITY_NOTICE
+/* (5) Notice: normal but significant condition */
+#define SYSLOG_SEVERITY_INFO
+/* (6) Informational: informational messages */
+#define SYSLOG_SEVERITY_DEBUG
+/* (7) Debug: debug-level messages */
+
+#define SYSLOG_FACILITY_KERN 0
+/* (0) kernel messages */
+#define SYSLOG_FACILITY_USER 1
+/* (1) user-level messages */
+#define SYSLOG_FACILITY_MAIL 2
+/* (2) mail system */
+#define SYSLOG_FACILITY_DAEMON 3
+/* (3) system daemons */
+#define SYSLOG_FACILITY_AUTH 4
+/* (4) security/authorization messages */
+#define SYSLOG_FACILITY_SYSLOG 5
+/* (5) messages generated internally by syslogd */
+#define SYSLOG_FACILITY_LPR 6
+/* (6) line printer subsystem */
+#define SYSLOG_FACILITY_NEWS 7
+/* (7) network news subsystem */
+#define SYSLOG_FACILITY_UUCP 8
+/* (8) UUCP subsystem */
+#define SYSLOG_FACILITY_CLOCK 9
+/* (9) clock daemon (and cron/at) */
+#define SYSLOG_FACILITY_AUTHPRIV 10
+/* (10) security/authorization messages */
+#define SYSLOG_FACILITY_FTP 11
+/* (11) FTP daemon */
+#define SYSLOG_FACILITY_NTP 12
+/* (12) NTP subsystem */
+#define SYSLOG_FACILITY_AUDIT 13
+/* (13) log audit */
+#define SYSLOG_FACILITY_ALERT 14
+/* (14) log alert */
+#define SYSLOG_FACILITY_CRON 15
+/* (15) clock daemon */
+#define SYSLOG_FACILITY_LOCAL0 16
+/* (16) local use 0  (local0) */
+#define SYSLOG_FACILITY_LOCAL1 17
+/* (17) local use 1  (local1) */
+#define SYSLOG_FACILITY_LOCAL2 18
+/* (18) local use 2  (local2) */
+#define SYSLOG_FACILITY_LOCAL3 19
+/* (19) local use 3  (local3) */
+#define SYSLOG_FACILITY_LOCAL4 20
+/* (20) local use 4  (local4) */
+#define SYSLOG_FACILITY_LOCAL5 21
+/* (21) local use 5  (local5) */
+#define SYSLOG_FACILITY_LOCAL6 22
+/* (22) local use 6  (local6) */
+#define SYSLOG_FACILITY_LOCAL7 23
+/* (23) local use 7  (local7) */
